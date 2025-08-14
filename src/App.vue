c6e0762c37f37f742271c8c102d2c2e1cfa10853
cara <cara091222@gmail.com>
2025-08-14 11:50:25 +0800

    0814_btn 更新

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,15 @@
 <template>
   <div class="home-page">
+    <AppHeader />
     <div class="vk">
-      <!-- <div class="container-share vk-container">
-        <div class="en-title-svg animation__el fadeUp">
-          <img src="@/assets/images/en-title.svg" alt="" />
-        </div>
-        <h1 class="title animation__el fadeUp delay__750">
-          國泰人壽專為<br />在台越籍設立的平台
-        </h1>
-        <h2 class="subtitle animation__el fadeUp delay__1000">
-          讓財富更有保障
-        </h2>
-      </div> -->
       <div class="vk-banner">
         <div class="swiper vk-swiper">
           <div class="swiper-wrapper vk-swiper-wrapper">
-            <div v-for="(item, index) in banner" :key="index" class="swiper-slide vk-swiper-slide">
+            <div
+              v-for="(item, index) in banner"
+              :key="index"
+              class="swiper-slide vk-swiper-slide"
+            >
               <div class="container-share vk-container">
                 <div class="en-title-svg animation__el fadeUp">
                   <img src="@/assets/images/en-title.svg" alt="" />
@@ -34,25 +28,63 @@
       </div>
       <!--Waves start-->
       <div class="waves-wrapper">
-        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-          viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+        <svg
+          class="waves"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          viewBox="0 24 150 28"
+          preserveAspectRatio="none"
+          shape-rendering="auto"
+        >
           <defs>
-            <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+            <path
+              id="gentle-wave"
+              d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+            />
           </defs>
           <g class="parallax">
-            <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.5" />
+            <use
+              xlink:href="#gentle-wave"
+              x="48"
+              y="0"
+              fill="rgba(255,255,255,0.5"
+            />
             <!-- <use
               xlink:href="#gentle-wave"
               x="48"
               y="3"
               fill="rgba(255,255,255,0.5)"
             /> -->
-            <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+            <use
+              xlink:href="#gentle-wave"
+              x="48"
+              y="5"
+              fill="rgba(255,255,255,0.3)"
+            />
             <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
           </g>
         </svg>
       </div>
       <!--Waves end-->
+    </div>
+    <div class="home-intro">
+      <div class="container-share">
+        <div class="home-intro-wrap">
+          <h2 class="title-main-share">
+            Bạn có gặp đủ chuyện lớn nhỏ khi sống ở Đài Loan mà chẳng biết hỏi
+            ai phải không?
+          </h2>
+          <div class="content">
+            <p
+              v-for="(item, index) in content"
+              :key="index"
+              class="list title-small-share"
+            >
+              {{ item.list }}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -66,32 +98,56 @@ import { Autoplay, EffectFade } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/effect-fade";
 import "@/assets/plugins/autoAnimation.js";
+import AppHeader from "./components/AppHeader.vue";
 
 const banner = [
   {
     src: new URL("@/assets/images/kv_banner.jpg", import.meta.url).href,
     alt: "Banner1",
-    title: "國泰人壽專為在台越籍設立的平台",
-    subtitle: "讓財富更有保障",
+    title: "cho người Việt Nam đang sinh sống tại Đài Loan",
+    subtitle: "Khởi đầu hành trình bảo vệ tài chính",
   },
   {
     src: new URL("@/assets/images/kv_banner02.jpg", import.meta.url).href,
     alt: "Banner2",
-    title: "Cathay Life xây dựng riêng một nền tảng dành cho người Việt Nam đang sinh sống tại Đài Loan ",
-    subtitle: "Khởi đầu hành trình bảo vệ tài chính vững chắc cho bạn, bắt đầu từ đây.",
+    title:
+      "Cathay Life xây dựng riêng một nền tảng dành cho người Việt Nam đang sinh sống tại Đài Loan ",
+    subtitle:
+      "Khởi đầu hành trình bảo vệ tài chính vững chắc cho bạn, bắt đầu từ đây.",
   },
   {
     src: new URL("@/assets/images/kv_banner03.jpg", import.meta.url).href,
     alt: "Banner3",
-    title: "國泰人壽專為在台越籍設立的平台",
-    subtitle: "讓財富更有保障",
+    title: "Cathay Life xây dựng riêng một nền tảng",
+    subtitle: "Khởi đầu hành trình bảo vệ tài chính",
+  },
+];
+
+const content = [
+  {
+    list: "Không biết xử lý thế nào những vấn đề trong cuộc sống ?",
+  },
+  {
+    list: "Đi đâu cũng gặp bất đồng về ngôn ngữ, sợ không hiểu sẽ dễ bị lừa?",
+  },
+  {
+    list: "Muốn gửi tiền về nhà, nhưng không biết chuyển tiền như nào?",
+  },
+  {
+    list: "Không biết làm thế nào để kê khai và nộp thuế, không đóng thuế kịp thời sẽ bị phạt tiền?",
+  },
+  {
+    list: "Thiếu cảm giác tin tưởng, không biết thông tin nào là chính xác?",
+  },
+  {
+    list: "Không hiểu biết rõ về pháp luật, khi gặp phải vấn đề không biết làm sao để bảo vệ quyền lợi của bản thân?",
   },
 ];
 
 onMounted(async () => {
   await nextTick();
 
-  new Swiper(".vk-swiper", {
+  const swiper = new Swiper(".vk-swiper", {
     modules: [Autoplay, EffectFade],
     loop: true,
     speed: 2000,
@@ -105,6 +161,7 @@ onMounted(async () => {
     },
   });
 });
+
 </script>
 
 <style lang="scss">
@@ -123,124 +180,178 @@ onMounted(async () => {
     @include media-down(tiny) {
       height: 600px;
     }
-  }
 
-  .vk-banner {
-    z-index: 2;
-    width: 100%;
-    height: 100%;
-    position: relative;
-    overflow: hidden;
-
-    .vk-swiper {
+    .vk-banner {
+      z-index: 2;
       width: 100%;
       height: 100%;
+      position: relative;
+      overflow: hidden;
 
-      .vk-swiper-wrapper {
+      .vk-swiper {
         width: 100%;
         height: 100%;
 
-        .vk-swiper-slide {
+        .vk-swiper-wrapper {
           width: 100%;
           height: 100%;
-          overflow: hidden;
-          position: relative;
 
-          &::before {
-            content: "";
-            position: absolute;
+          .vk-swiper-slide {
             width: 100%;
             height: 100%;
-            top: 0;
-            left: 0;
-            background: linear-gradient(90deg,
-                rgba(0, 0, 0, 0.5) 6.68%,
-                rgba(0, 0, 0, 0) 73.78%);
-            z-index: 2;
+            overflow: hidden;
+            position: relative;
 
-            @include media-down(xxl) {
-              background: #00000050;
-            }
-          }
-
-          .vk-container {
-            @include poa-center;
-            z-index: 5;
-            width: 90%;
-
-            .en-title-svg {
+            &::before {
+              content: "";
+              position: absolute;
               width: 100%;
               height: 100%;
-              max-width: 755px;
+              top: 0;
+              left: 0;
+              background: linear-gradient(
+                90deg,
+                rgba(0, 0, 0, 0.5) 6.68%,
+                rgba(0, 0, 0, 0) 73.78%
+              );
+              z-index: 2;
 
               @include media-down(xxl) {
-                margin: auto;
+                background: #00000050;
               }
+            }
 
-              img {
+            .vk-container {
+              @include poa-center;
+              z-index: 5;
+              width: 90%;
+
+              .en-title-svg {
                 width: 100%;
                 height: 100%;
-                object-fit: cover;
+                max-width: 755px;
+
+                @include media-down(xxl) {
+                  margin: auto;
+                }
+
+                img {
+                  width: 100%;
+                  height: 100%;
+                  object-fit: cover;
+                }
+              }
+
+              .title {
+                @include fz(46);
+                color: var(--color-white);
+                font-style: normal;
+                font-weight: 700;
+                line-height: 52px;
+                letter-spacing: 2px;
+                margin: 38px 0 20px 0;
+                width: 60%;
+
+                @include media-down(xxl) {
+                  @include fz(40);
+                  line-height: 48px;
+                  text-align: center;
+                  width: 100%;
+                }
+
+                @include media-down(lg) {
+                  @include fz(36);
+                  line-height: 40px;
+                }
+
+                @include media-down(sm) {
+                  @include fz(30);
+                  line-height: 34px;
+                  margin: 20px 0;
+                }
+              }
+
+              .subtitle {
+                color: var(--color-white);
+                font-size: 32px;
+                font-style: normal;
+                font-weight: 400;
+                line-height: normal;
+                letter-spacing: 2px;
+                width: 60%;
+
+                @include media-down(xxl) {
+                  text-align: center;
+                  width: 100%;
+                }
+
+                @include media-down(lg) {
+                  @include fz(22);
+                }
               }
             }
 
-            .title {
-              @include en-fontF;
-              @include fz(46);
-              color: var(--color-white);
-              font-style: normal;
-              font-weight: 700;
-              line-height: 58px;
-              letter-spacing: 2px;
-              margin: 43px 0 20px 0;
-              width: 60%;
-
-              @include media-down(xxl) {
-                @include fz(40);
-                line-height: 48px;
-                text-align: center;
-                width: 100%;
-              }
-
-              @include media-down(lg) {
-                @include fz(36);
-                line-height: 40px;
-              }
-
-              @include media-down(sm) {
-                @include fz(30);
-                line-height: 34px;
-                margin: 20px 0;
-              }
-            }
-
-            .subtitle {
-              @include en-fontF;
-              color: var(--color-white);
-              font-size: 32px;
-              font-style: normal;
-              font-weight: 400;
-              line-height: normal;
-              letter-spacing: 2px;
-              width: 60%;
-
-              @include media-down(xxl) {
-                text-align: center;
-                width: 100%;
-              }
-
-              @include media-down(lg) {
-                @include fz(22);
-              }
+            img {
+              width: 100%;
+              height: 100%;
+              object-fit: cover;
+              object-position: 80% 0;
             }
           }
+        }
+      }
+    }
+  }
 
-          img {
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-            object-position: 80% 0;
-          }
+  // Home Intro
+  .home-intro {
+    width: 100%;
+    height: 100%;
+    padding: 100px 0;
+    
+
+    @include media-down(lg) {
+      padding: 80px 0;
+    }
+
+    @include media-down(sm) {
+      padding: 50px 0;
+    }
+
+    .container-share {
+      @include d-flex(center, center, column);
+    }
+
+    &-wrap {
+      @include d-flex(center, center, column);
+      max-width: 845px;
+      padding: 100px;
+      border-radius: 50px;
+      background: rgba(255, 255, 255, 0.2);
+      box-shadow: 0 0 31px 0 rgba(23, 115, 56, 0.2);
+      text-align: center;
+
+      @include media-down(xl) {
+        padding: 80px;
+      }
+
+      @include media-down(md) {
+        padding: 50px;
+      }
+      @include media-down(md) {
+        padding: 30px;
+      }
+
+      .content {
+        padding-top: 40px;
+
+        .list {
+          font-weight: 400;
+          color: var(--color-grey);
+        }
+
+        .list + .list {
+          padding-top: 18px;
         }
       }
     }
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,243 @@
+<template>
+  <div class="app-header">
+    <div class="app-container">
+      <div class="container-share">
+        <div class="app-logo">
+          <img src="@/assets/images/logo.svg" alt="" />
+        </div>
+        <nav class="app-nav">
+          <ul class="nav-wrap">
+            <li class="main-item">
+              <a class="main-link" data-hash="#">về chúng tôi</a>
+            </li>
+            <li class="main-item">
+              <a class="main-link" data-hash="#">Dịch vụ</a>
+            </li>
+            <li class="main-item">
+              <a class="main-link" data-hash="#">tư vấn</a>
+            </li>
+            <li class="main-item">
+              <a class="main-link" data-hash="#">đặt vấn đề</a>
+            </li>
+          </ul>
+          <div class="contact-btn">
+            <a href="" class="btn-main">Liên hệ</a>
+          </div>
+          <div class="nav-hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import jQuery from "jquery";
+
+export default {
+  mounted() {
+    const $ = jQuery;
+    // 主選單點擊時，只加在 main-link 上
+    jQuery(".main-link").on("click", function (e) {
+      jQuery(".main-link").removeClass("active");
+      jQuery(this).addClass("active");
+    });
+
+    // 點擊漢堡選單
+    jQuery(".nav-hamburger").on("click", function (e) {
+      e.stopPropagation();
+      jQuery(this).toggleClass("active");
+      jQuery(".app-container").toggleClass("menu-show"); // 控制 nav-wrap 出現
+    });
+
+    // 點擊 page-top 回到頁面頂端
+    jQuery(".page-top").on("click", function (e) {
+      e.preventDefault(); // 阻止 <a href="#"> 的預設跳轉行為
+      jQuery("html, body").animate({ scrollTop: 0 }, 600);
+    });
+
+  },
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/styles/main.scss";
+
+.app-header {
+  position: fixed;
+  z-index: 999;
+  width: 100%;
+  max-height: 80px;
+  transition: all 0.5s ease;
+
+  .app-container {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    padding: 20px 0;
+    background: var(--color-white);
+    box-shadow: 0 0 31px 0 rgba(23, 115, 56, 0.2);
+
+    @include media-down(sm) {
+      padding: 15px 0;
+    }
+  }
+
+  .container-share {
+    @include d-flex(space-between, center);
+  }
+
+  .app-logo {
+    width: 150px;
+    height: 100%;
+
+    img {
+      width: 100%;
+      height: 100%;
+      @include d-flex(center, center, column);
+    }
+
+    @include media-down(sm) {
+      width: 120px;
+    }
+  }
+
+  .app-container.menu-show .nav-wrap {
+    transform: translateX(0);
+  }
+
+  .app-nav {
+    @include d-flex(center, center);
+
+    .nav-wrap {
+      @include d-flex(center, center);
+      gap: 25px;
+
+      @include media-down(lg) {
+        gap: 15px;
+      }
+
+      @include media-down(md) {
+        flex-direction: column;
+        justify-content: left;
+        position: absolute;
+        top: -20px;
+        right: 0;
+        padding-top: 78px;
+        width: 50%;
+        height: 100vh;
+        background-color: #ffffff;
+        transform: translateX(100%);
+        transition: transform 0.3s ease;
+      }
+
+      .main-link {
+        font-weight: 700;
+        padding: 10px 0;
+        position: relative;
+
+        @include media-down(md) {
+          padding: 5px 0;
+        }
+
+        &::after {
+          content: "";
+          position: absolute;
+          bottom: 0;
+          left: 0;
+          width: 0;
+          height: 3px;
+          background: var(--color-main);
+          transition: all 0.5s ease-in-out;
+        }
+
+        &:hover::after {
+          width: 100%;
+          background: var(--color-main);
+        }
+
+        &.active::after {
+          width: 100%;
+        }
+      }
+    }
+
+    .contact-btn {
+      margin-left: 25px;
+      box-sizing: border-box;
+
+
+      @include media-down(lg) {
+        gap: 15px;
+      }
+    }
+
+    // 漢堡選單
+    .nav-hamburger {
+      display: none;
+
+      @include media-down(md) {
+        display: block;
+        transition: all 0.5s ease-in-out;
+        position: relative;
+        width: 20px;
+        height: 20px;
+        margin-left: 10px;
+        // background: var(--color-main);
+        // border-radius: 100%;
+        cursor: pointer;
+        top: 0rem;
+
+        // @include media-down(sm) {
+        //   top: 1rem;
+        // }
+
+        > span {
+          width: 20px;
+          height: 3px;
+          position: absolute;
+          background: var(--color-main);
+          right: 0;
+          margin: auto;
+          left: 0;
+          border-radius: 50px;
+          transition: all 0.5s cubic-bezier(0.5, 0, 0.1, 1.2);
+          top: calc(50% - ((5.5px)) + -1px);
+        }
+
+        > span:nth-child(1) {
+          top: calc(50% - ((5.5px)) + -3px);
+        }
+
+        > span:nth-child(2) {
+          top: calc(50% - ((5.5px)) + 3px);
+        }
+
+        > span:nth-child(3) {
+          top: calc(50% - ((5.5px)) + 9px);
+        }
+      }
+
+      &.active span:nth-child(1) {
+        transform: rotate(45deg);
+        top: 9px;
+        background: var(--color-black);
+      }
+
+      &.active span:nth-child(2) {
+        opacity: 0;
+      }
+
+      &.active span:nth-child(3) {
+        transform: rotate(-45deg);
+        top: 9px;
+        background: var(--color-black);
+      }
+    }
+  }
+}
+</style>
